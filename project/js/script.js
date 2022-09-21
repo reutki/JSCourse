f4996e39c40b3ace2a4a2e68a46cf9cdfdb33f78
reutki <marcu.reutki@gmail.com>
2022-09-21 20:47:56 +0300

    finished homework in lesson nr 33

--- project/js/script.js
+++ project/js/script.js
@@ -1,6 +1,6 @@
 /* Задания на урок:
 
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+1)//? Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
 новый фильм добавляется в список. Страница не должна перезагружаться.
 Новый фильм должен добавляться в movieDB.movies.
 Для получения доступа к значению input - обращаемся к нему как input.value;
@@ -10,10 +10,10 @@ P.S. Здесь есть несколько вариантов решения з
 
 3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+4)//? Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
 "Добавляем любимый фильм"
 
-5) Фильмы должны быть отсортированы по алфавиту */
+5)//? Фильмы должны быть отсортированы по алфавиту */
 
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
@@ -29,6 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const advertising = document.querySelectorAll(".promo__adv img");
   const genres = document.querySelector(".promo__genre");
+  const movieList = document.querySelector(".promo__interactive-list");
   const bg = document.querySelector(".promo__bg");
   const form = document.querySelector("form.add");
   const formInput = document.querySelector(".adding__input");
@@ -37,14 +38,21 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const newMovie = formInput.value;
+    let newMovie = formInput.value;
     const favourite = checkbox.checked;
-    if (favourite && newMovie != "" && newMovie != null && newMovie != NaN) {
+    if (newMovie) {
+      if (newMovie.length > 21) {
+        newMovie = `${newMovie.substring(0, 22)}...`;
+        //substring(change from (initial,final))
+      }
       movieDB.movies.push(newMovie);
+      createMovieList(movieDB.movies, movieList);
       sortArr(movieDB.movies);
-      createMovieList(movieDB.movies, movieDB);
       e.target.reset();
       console.log(movieDB.movies);
+      if (favourite) {
+        console.log("added favourite movie!");
+      }
     }
   });
 
@@ -72,11 +80,17 @@ document.addEventListener("DOMContentLoaded", () => {
       <div class="delete"></div>
     </li>`;
     });
+    document.querySelectorAll(".delete").forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        btn.parentElement.remove();
+        movieDB.movies.splice(i, 1);
+        createMovieList(films, parent);
+      });
+    });
   }
 
   makeChanges();
-  deleteAdv(advertising);
-  sortArr(movieDB.movies);
-  // createMovieList(movieDB.movies, movieDB);
+  createMovieList(movieDB.movies, movieList);
   console.log(movieDB.movies);
+  deleteAdv(advertising);
 });
