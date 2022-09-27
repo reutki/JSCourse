f2a4ff440d667d25fd8ae0563ffc87c0a00bbed3
reutki <marcu.reutki@gmail.com>
2022-09-27 13:42:46 +0300

    working with dates

---
+++ 040 script.js
@@ -0,0 +1,18 @@
+const now = new Date();
+console.log(now.setHours(18));
+console.log(now);
+// console.log(now.getFullYear());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getSeconds());
+
+let start = new Date();
+
+for (let i = 0; i < 100000000; i++) {
+  let some = i ** 3; //power operator is **
+}
+
+let end = new Date();
+
+alert(`Cicle worked in ${end - start} miliseconds`);
